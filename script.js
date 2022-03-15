89e2019bcad5c1e1ec09fd92f72cc00e4e199144
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-03-15 20:25:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// window.alert("Hello");
+
+// const response = window.confirm("are you sure you want to leave?");
+// console.log(response);
+
+// const user = window.prompt("Enter your name");
+// console.log(user);
+
+const colors = ["red", "blue", "green", "pink", "yellow", "black"];
+
+let index = 0;
+
+const cycleColors = () => {
+  console.log("cycleColors");
+  document.body.style.backgroundColor = colors[index];
+  index++;
+  if (index === colors.length) {
+    index = 0;
+  }
+};
+let colorInterval;
+function clicked() {
+  colorInterval = setInterval(cycleColors, 1000);
+}
+function stop() {
+  console.log("Stop");
+  clearInterval(colorInterval);
+}
